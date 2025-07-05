45cc6bcc909abc3912e9b5861d4c289b6c647224
EGL-GPQ <sadsongslimited@gmail.com>
2025-07-05 22:06:17 +0600

    Update layout.jsx

--- app/app/layout.jsx
+++ app/app/layout.jsx
@@ -1,3 +1,4 @@
+// app/layout.jsx
 import React from 'react';
 
 export const metadata = {
@@ -8,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head />
       <body>{children}</body>
     </html>
   );
